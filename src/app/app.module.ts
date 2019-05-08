2375a88ebffade00fe5dd6213d4894fca9d3f855
turbek <turbek93@gmail.com>
2019-05-08 08:57:54 +0200

    add dependencies and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+
+import {ReactiveFormsModule} from '@angular/forms';
+import {NavbarComponent} from './navbar/navbar.component';
+import {SearchInputComponent} from './search-input/search-input.component';
+import {CardComponent} from './card-holder/card/card.component';
+import {CardHolderComponent} from './card-holder/card-holder.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        SearchInputComponent,
+        CardComponent,
+        CardHolderComponent,
+        LandingPageComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
