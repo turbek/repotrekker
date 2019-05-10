bc29a737f6f6f05e207ed0e2c485c2bc92a8b09f
turbek <turbek93@gmail.com>
2019-05-10 22:32:50 +0200

    add progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 import {ReactiveFormsModule} from '@angular/forms';
 import {NavbarComponent} from './navbar/navbar.component';
@@ -12,8 +12,11 @@ import {CardComponent} from './card-holder/card/card.component';
 import {CardHolderComponent} from './card-holder/card-holder.component';
 import {LandingPageComponent} from './landing-page/landing-page.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { IssueHolderComponent } from './issues-holder/issue-holder.component';
-import { IssueComponent } from './issues-holder/issue/issue.component';
+import {IssueHolderComponent} from './issues-holder/issue-holder.component';
+import {IssueComponent} from './issues-holder/issue/issue.component';
+import {MatProgressBarModule} from "@angular/material";
+import {ProgressBarComponent} from './progress-bar/progress-bar.component';
+import {ProgressBarInterceptorService} from "./interceptor/progress-bar-interceptor.service";
 
 @NgModule({
     declarations: [
@@ -24,16 +27,24 @@ import { IssueComponent } from './issues-holder/issue/issue.component';
         CardHolderComponent,
         LandingPageComponent,
         IssueHolderComponent,
-        IssueComponent
+        IssueComponent,
+        ProgressBarComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
         ReactiveFormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        MatProgressBarModule
     ],
-    providers: [],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ProgressBarInterceptorService,
+            multi: true
+        }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
